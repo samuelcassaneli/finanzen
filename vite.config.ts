fb5bc47e50bdee7c1372d88227992cc0fb937941
samuelcassaneli <samuelcassaneli@proton.me>
2025-11-03 15:11:54 -0300

    feat

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      css: {
+        postcss: './postcss.config.js',
+      },
       plugins: [
         react(),
         VitePWA({
