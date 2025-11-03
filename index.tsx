5c74aee01d157137eeec88e83bf999a5b8c7f2d1
samuelcassaneli <samuelcassaneli@proton.me>
2025-11-03 15:07:43 -0300

    feat

--- index.tsx
+++ index.tsx
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch(error => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-  });
-}
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
